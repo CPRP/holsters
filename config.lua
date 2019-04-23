e2e62679b439ba0eb8412588922c475ebf4d51fe
Jam <jamesc2400@gmail.com>
2019-04-23 17:19:20 +0100

    Expand config to include more default peds

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 config = {
-  ["mp_m_freemode_01"] = {
+  ["mp_m_freemode_01"] = { -- Male multiplayer ped
     ["variations"] = {
-     [7] = {
+      [7] = { -- Simple Trainer: 'Teeth', Lambda: 'Neck'
         [1] = 3,
         [6] = 5,
         [8] = 2,
@@ -11,8 +11,8 @@ config = {
       }
     }
   },
-  ["mp_f_freemode_01"] = {
-    ["variations"]= {
+  ["mp_f_freemode_01"] = { -- Female multiplayer ped
+    ["variations"] = { -- Simple Trainer: 'Teeth', Lambda: 'Neck'
       [7] = {
         [1] = 3,
         [6] = 5,
@@ -21,5 +21,26 @@ config = {
         [81] = 82
       }
     }
-  }
+  },
+  ["s_m_y_hwaycop_01"] = {
+    ["variations"] = {
+      [9] = {
+        [1] = 0
+      }
+    }
+  },
+  ["s_m_y_cop_01"] = {
+    ["variations"] = {
+      [9] = {
+        [1] = 0
+      }
+    }
+  },
+  ["s_m_y_sheriff_01"] = {
+    ["variations"] = {
+      [9] = {
+        [1] = 0
+      }
+    }
+  },
 }
