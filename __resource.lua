fbc5e13fe91383d228379994fc5c8bdacc90fd5e
James <35628223+Jamelele@users.noreply.github.com>
2019-04-23 21:08:17 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,4 +1,6 @@
-client_script 'client.lua'
-client_script 'config.lua'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-files 'holsters.json'
+client_scripts {
+  'client.lua',
+  'config.lua'
+}
