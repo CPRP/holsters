d984faae0bfc32db708879e0db5473e1702d00c7
Jamelele <35628223+Jamelele@users.noreply.github.com>
2020-05-26 11:49:25 +0000

    Just remove 'last updated' comment
    this is a git repo after all

--- config.lua
+++ config.lua
@@ -13,7 +13,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 --]]
 
--- Created by Jamelele, last updated March 2020
+-- Created by Jamelele
 
 -- Take a look at the documentation for configuring:
 -- https://github.com/Jamelele/holsters/wiki/Configuration-Guide
